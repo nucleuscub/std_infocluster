2b9eae9b1208530c62f44b18c5860729e7f91a3e
Ramon Martinez Cancino <ramonmc@ucsd.edu>
2015-05-12 14:11:29 -0700

    Adapted to pull values from design.cell

--- std_clustat.m
+++ std_clustat.m
@@ -82,22 +82,43 @@ for imeasure = 1:length(STUDY.etc.preclust.preclustparams);
     end;
     % Loop per clusters
     for cls = 1 : length(clustinfo)
+            
+%         filename = {clustinfo(cls).datasetinfo{1}.filename};
+%         pathfile = {clustinfo(cls).datasetinfo{1}.filepath};
+%         
+%         for i = 1: length({clustinfo(cls).datasetinfo{1}.filename})
+%             if any([strcmp(strcom,'erp'),strcmp(strcom,'spec'),strcmp(strcom,'ersp'),strcmp(strcom,'itc')])
+%                 [tmp,filetmp,tmp] = fileparts(filename{i});
+%                 file2read(i).filebase = [pathfile{i} filesep filetmp];
+%                 
+%             elseif strcmp(strcom,'scalp')
+%                 file2read(i).filepath  = pathfile{i};
+%                 file2read(i).filename  = filename{i};
+%             end
+%         end
         
+        % Retreiving datasets (long story while using cell.design)
+        % Getting ICs
+        clsindx = find(strcmp(clustinfo(cls).name,{STUDY.cluster.name}));
+        tmpval = cell2mat(STUDY.cluster(clsindx).allinds)';
+        clustinfo(cls).ics = tmpval(:)'; clear tmpval;
         
-        filename = {clustinfo(cls).datasetinfo{1}.filename};
-        pathfile = {clustinfo(cls).datasetinfo{1}.filepath};
+        % getting sets
+        tmpval = cell2mat(STUDY.cluster(clsindx).setinds)';
+        cellinds = tmpval(:)'; clear tmpval;
+        file2read_tmp = {STUDY.design(STUDY.currentdesign).cell(cellinds).filebase}';
         
-        for i = 1: length({clustinfo(cls).datasetinfo{1}.filename})
-            if any([strcmp(strcom,'erp'),strcmp(strcom,'spec'),strcmp(strcom,'ersp'),strcmp(strcom,'itc')])
-                [tmp,filetmp,tmp] = fileparts(filename{i});
-                file2read(i).filebase = [pathfile{i} filesep filetmp];
-                
-            elseif strcmp(strcom,'scalp')
-                file2read(i).filepath  = pathfile{i};
-                file2read(i).filename  = filename{i};
+        if any([strcmp(strcom,'erp'),strcmp(strcom,'spec'),strcmp(strcom,'ersp'),strcmp(strcom,'itc')])
+            file2read = cell2struct(file2read_tmp,'filebase',size(file2read_tmp,1));     
+        elseif strcmp(strcom,'scalp')
+            for  i = 1: size(file2read_tmp,1)
+                [filepath, filename, tmp] = fileparts(file2read_tmp{i});clear tmp;
+                file2read(i).filepath  = filepath;
+                file2read(i).filename  = filename;
             end
         end
-        
+        clear file2read_tmp setinds clsindx;
+         
         switch strcom
             
             % select ica component ERPs
